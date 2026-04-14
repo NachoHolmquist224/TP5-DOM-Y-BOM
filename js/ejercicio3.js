c175c3c6cd43760af416fe123e2f6626b71247d3
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-04-14 15:55:06 -0300

    ejercicio3 terminado

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -2,9 +2,25 @@ function creartarea(e) {
   e.preventDefault();
   const tarea = document.getElementById("tarea").value;
   const ul = document.querySelector(".list-group");
-  ul.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">${tarea} <button class="btn btn-danger">Borrar Tarea</button></li>`;
+  ul.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            ${tarea} 
+            <button class="btn btn-danger btn-borrar">Borrar Tarea</button>
+        </li>`;
   formtarea.reset();
 }
 
 const formtarea = document.getElementById("formtarea");
+const listaTareas = document.querySelector(".list-group");
+
 formtarea.addEventListener("submit", creartarea);
+
+// --- LÓGICA PARA ELIMINAR ---
+listaTareas.addEventListener("click", (e) => {
+  // Verificamos si el elemento clickeado tiene la clase de nuestro botón
+  if (e.target.classList.contains("btn-borrar")) {
+    // 'e.target' es el botón. 'parentElement' es el <li> que lo contiene.
+    const liParaEliminar = e.target.parentElement;
+    liParaEliminar.remove();
+  }
+});
